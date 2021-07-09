3929cef1cc88eb6ae83d3783ac4065aaf29bce83
juju2181 <anishshilpakar8@gmail.com>
2021-07-09 09:19:15 +0545

    Day 4 codes added

---
+++ Day4/SF21-APIs/README.md
@@ -0,0 +1,25 @@
+# SF21-APIs
+This project contains the starter files and slides for the fourth day of Software Fellowship 2021
+
+### Instructions
+
+1. Download zip or clone this repository
+2. Create a virtual environment using following command in the terminal.
+   ```sh
+   python -m venv env
+   ```
+3. Activate virtual environment 
+   1. For Windows ( Powershell ): 
+      ```powershell
+      env\Scripts\activate.bat
+      ```
+   2. For GNU/Linux : 
+      ```sh
+      source ./env/bin/activate
+      ```
+4. Install `flask` and `requests`:
+   ```sh
+   pip install flask requests
+   ```
+6. Open the project in [Visual Studio Code (VSCode)](https://code.visualstudio.com/)
+7. Download [Thunder Client](https://www.thunderclient.io/) extension in VSCode itself from **Extensions Tab**
---
+++ Day4/SF21-APIs/working-with-api/app.py
@@ -0,0 +1,71 @@
+# Create virtual environment
+# $ python3 -m pip install flask
+# $ python3 -m pip install requests
+# Get api key from https://the-one-api.dev
+
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config.py')
+
+API_KEY = app.config['API_KEY']
+
+@app.route('/')
+def get_home():
+    return render_template('base.html')
+
+@app.route('/books', methods=['GET'])
+def get_books():
+    # Get books from api : https://the-one-api.dev/v2/book
+    response = requests.get("https://the-one-api.dev/v2/book")
+    extracted_data = response.json()
+    # print(extracted_data)
+    books = extracted_data['docs']
+    # Convert response object to dict 
+
+    # return jsonify(books)
+    return render_template('table.html', type="Books", data=books)
+
+
+@app.route('/movies', methods=["GET"])
+def get_movies():
+    # Set Authentication Header
+    #Header is needed for token for api key
+    headers = {"Authorization": f'Bearer {API_KEY}'}
+
+    # Get Movies from api: https://the-one-api.dev/v2/movie
+    response = requests.get("https://the-one-api.dev/v2/movie",
+                            headers=headers)
+
+    extracted_data = response.json()
+    movies = extracted_data["docs"]
+
+    return render_template('table.html', type="Movies", data=movies)
+
+
+@app.route('/characters', methods=["GET"])
+def get_characters():
+
+    # Set Authentication Header
+
+    headers = {"Authorization": f'Bearer {API_KEY}'}
+
+    # Get Characters from api: https://the-one-api.dev/v2/character
+
+    response = requests.get("https://the-one-api.dev/v2/character",headers=headers)
+
+    extracted_data = response.json()
+    characters = extracted_data["docs"]
+
+    return render_template('table.html', type='Characters', data=characters)
+
+@app.route('/players',methods=["GET"])
+def get_players():
+    response = requests.get("http://localhost:5001")
+    extracted_data = response.json()
+    return render_template('table.html',type="Players",data=extracted_data)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
